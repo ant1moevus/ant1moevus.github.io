d26882b8f0107895e129a77bb8afde3ab2cb301d
Prof Moevus <106986122+prof-moevus@users.noreply.github.com>
2024-10-06 00:09:46 -0400

    💄 Add general style and blog colors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use yew::prelude::*;
-
+const PRIMARY_COLOR: &str = "emerald-900";
+const SECONDARY_COLOR: &str = "amber-200";
+const ACCENT_COLOR: &str = "zinc-900";
 #[function_component]
 fn App() -> Html {
     let counter = use_state(|| 0);
@@ -11,13 +13,16 @@ fn App() -> Html {
         }
     };
 
-    let btn_classes = String::from("bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4");
+    let btn_classes = String::from("bg-zinc-900 hover:bg-amber-600 text-amber-200 font-bold py-2 px-4");
 
     html!{
         <>
+            <h2 class={classes!("bg-zinc-900", "text-amber-200", "text-4xl", "text-center", "p-5")}>{"Antoine Moevus"}</h2>
+            <br/>
             <button {onclick}  class={classes!(btn_classes)}>{ " +1 " }</button>
-            <p>{ *counter }</p>
-            <p class={classes!("bg-red-100")}>{"Test!"}</p>
+            <p class={classes!("text-amber-100", "text-xl", "text-center")}>{ *counter }</p>
+        <br/>
+            <p class={classes!("bg-amber-900", "text-amber-100", "text-4xl", "text-center")}>{"Work in progress"}</p>
         </>
     }
 }
