4c42c935ca37a2404ef3ed3bf764dd276a31e24c
Prof Moevus <106986122+prof-moevus@users.noreply.github.com>
2024-10-05 21:55:44 -0400

    💄 Add style for button

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,14 @@ fn App() -> Html {
         }
     };
 
+    let btn_classes = String::from("bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4");
+
     html!{
-        <div>
-            <button {onclick}>{ "+1" }</button>
+        <>
+            <button {onclick}  class={classes!(btn_classes)}>{ " +1 " }</button>
             <p>{ *counter }</p>
             <p class={classes!("bg-red-100")}>{"Test!"}</p>
-        </div>
+        </>
     }
 }
 
